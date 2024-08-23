039f1237a93b4fbebb5ccdf4ee37337763f08475
Mateo González Zugasti <45539970+mateogz@users.noreply.github.com>
2024-08-23 11:51:55 -0300

    Update __manifest__.py

--- web_action_conditionable/__manifest__.py
+++ web_action_conditionable/__manifest__.py
@@ -1,9 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "web_action_conditionable",
-    "version": "16.0.1.0.0",
-    "depends": ["base", "web"],
-    "data": [],
+
     "author": "Cristian Salamea,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
